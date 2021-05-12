df0542ab756171b7f58d291c8aee01bfabfce04a
slisaasquatch <sli@saasquat.ch>
2021-05-11 21:33:03 -0700

    cleanup

--- src/main/java/com/saasquatch/rhinojsonata/JSONata.java
+++ src/main/java/com/saasquatch/rhinojsonata/JSONata.java
@@ -1,5 +1,6 @@
 package com.saasquatch.rhinojsonata;
 
+import static com.saasquatch.rhinojsonata.JunkDrawer.TIMEBOX_EXPRESSION_JS;
 import static com.saasquatch.rhinojsonata.JunkDrawer.readerToString;
 import static com.saasquatch.rhinojsonata.JunkDrawer.rethrowRhinoException;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -145,41 +146,7 @@ public final class JSONata {
        * The code comes from https://github.com/jsonata-js/jsonata/blob/97295a6fdf0ed0df7677e5bf36a50bb633eb53a2/test/run-test-suite.js#L158
        * It is licenced under MIT License
        */
-      cx.evaluateString(scope, ""
-          + "function timeboxExpression(expr, timeout, maxDepth) {\n"
-          + "    var depth = 0;\n"
-          + "    var time = Date.now();\n"
-          + "\n"
-          + "    var checkRunnaway = function() {\n"
-          + "        if (depth > maxDepth) {\n"
-          + "            // stack too deep\n"
-          + "            throw {\n"
-          + "                message:\n"
-          + "                    \"Stack overflow error: Check for non-terminating recursive function.  Consider rewriting as tail-recursive.\",\n"
-          + "                stack: new Error().stack,\n"
-          + "                code: \"U1001\"\n"
-          + "            };\n"
-          + "        }\n"
-          + "        if (Date.now() - time > timeout) {\n"
-          + "            // expression has run for too long\n"
-          + "            throw {\n"
-          + "                message: \"Expression evaluation timeout: Check for infinite loop\",\n"
-          + "                stack: new Error().stack,\n"
-          + "                code: \"U1001\"\n"
-          + "            };\n"
-          + "        }\n"
-          + "    };\n"
-          + "\n"
-          + "    // register callbacks\n"
-          + "    expr.assign(\"__evaluate_entry\", function() {\n"
-          + "        depth++;\n"
-          + "        checkRunnaway();\n"
-          + "    });\n"
-          + "    expr.assign(\"__evaluate_exit\", function() {\n"
-          + "        depth--;\n"
-          + "        checkRunnaway();\n"
-          + "    });\n"
-          + "}", null, 1, null);
+      cx.evaluateString(scope, TIMEBOX_EXPRESSION_JS, null, 1, null);
       ScriptableObject.callMethod(scope, "timeboxExpression",
           new Object[]{jsonataObject, options.timeout, options.maxDepth});
     }
--- src/main/java/com/saasquatch/rhinojsonata/JunkDrawer.java
+++ src/main/java/com/saasquatch/rhinojsonata/JunkDrawer.java
@@ -11,6 +11,46 @@ import org.mozilla.javascript.Scriptable;
 
 final class JunkDrawer {
 
+  /**
+   * The code comes from https://github.com/jsonata-js/jsonata/blob/97295a6fdf0ed0df7677e5bf36a50bb633eb53a2/test/run-test-suite.js#L158
+   * It is licenced under MIT License
+   */
+  public static final String TIMEBOX_EXPRESSION_JS = ""
+      + "function timeboxExpression(expr, timeout, maxDepth) {\n"
+      + "    var depth = 0;\n"
+      + "    var time = Date.now();\n"
+      + "\n"
+      + "    var checkRunnaway = function() {\n"
+      + "        if (depth > maxDepth) {\n"
+      + "            // stack too deep\n"
+      + "            throw {\n"
+      + "                message:\n"
+      + "                    \"Stack overflow error: Check for non-terminating recursive function.  Consider rewriting as tail-recursive.\",\n"
+      + "                stack: new Error().stack,\n"
+      + "                code: \"U1001\"\n"
+      + "            };\n"
+      + "        }\n"
+      + "        if (Date.now() - time > timeout) {\n"
+      + "            // expression has run for too long\n"
+      + "            throw {\n"
+      + "                message: \"Expression evaluation timeout: Check for infinite loop\",\n"
+      + "                stack: new Error().stack,\n"
+      + "                code: \"U1001\"\n"
+      + "            };\n"
+      + "        }\n"
+      + "    };\n"
+      + "\n"
+      + "    // register callbacks\n"
+      + "    expr.assign(\"__evaluate_entry\", function() {\n"
+      + "        depth++;\n"
+      + "        checkRunnaway();\n"
+      + "    });\n"
+      + "    expr.assign(\"__evaluate_exit\", function() {\n"
+      + "        depth--;\n"
+      + "        checkRunnaway();\n"
+      + "    });\n"
+      + "}";
+
   private JunkDrawer() {}
 
   public static <T> T rethrowRhinoException(Context cx, Scriptable scope, RhinoException e) {
