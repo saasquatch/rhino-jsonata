6c083928998da238e031588f3f9372771e647524
slisaasquatch <sli@saasquat.ch>
2021-05-12 20:31:29 -0700

    Update JSONataExpressionTests.java

--- src/test/java/com/saasquatch/rhinojsonata/JSONataExpressionTests.java
+++ src/test/java/com/saasquatch/rhinojsonata/JSONataExpressionTests.java
@@ -1,9 +1,11 @@
 package com.saasquatch.rhinojsonata;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import org.junit.jupiter.api.Test;
 
 public class JSONataExpressionTests {
@@ -23,6 +25,18 @@ public class JSONataExpressionTests {
       assertTrue(e.getMessage().contains("before end of expression"));
     }
     assertDoesNotThrow(() -> JSONataExpression.parse("$foo"));
+  }
+
+  @Test
+  public void testEvaluateBasic() {
+    assertEquals(JsonNodeFactory.instance.missingNode(),
+        JSONataExpression.parse("foo").evaluate(null));
+    assertEquals(JsonNodeFactory.instance.missingNode(),
+        JSONataExpression.parse("foo").evaluate(JsonNodeFactory.instance.textNode("foo")));
+    assertEquals(JsonNodeFactory.instance.nullNode(),
+        JSONataExpression.parse("null").evaluate(JsonNodeFactory.instance.textNode("foo")));
+    assertEquals(JsonNodeFactory.instance.objectNode(),
+        JSONataExpression.parse("{\"foo\":bar}").evaluate(null));
   }
 
 }
