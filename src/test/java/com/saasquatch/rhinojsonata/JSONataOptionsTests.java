20bb8c8e2836cf8abad5f31e1da14abc363ec85b
slisaasquatch <sli@saasquat.ch>
2021-05-12 22:54:17 -0700

    Update JSONataOptionsTests.java

--- src/test/java/com/saasquatch/rhinojsonata/JSONataOptionsTests.java
+++ src/test/java/com/saasquatch/rhinojsonata/JSONataOptionsTests.java
@@ -1,7 +1,15 @@
 package com.saasquatch.rhinojsonata;
 
+import static com.saasquatch.rhinojsonata.JunkDrawer.readerToString;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 import org.junit.jupiter.api.Test;
 
 public class JSONataOptionsTests {
@@ -12,6 +20,36 @@ public class JSONataOptionsTests {
     final JSONataOptions.Builder builder = JSONataOptions.newBuilder();
     assertThrows(NullPointerException.class, () -> builder.setObjectMapper(null));
     assertThrows(NullPointerException.class, () -> builder.setJSONataJsSource(null));
+  }
+
+  @Test
+  public void testCustomObjectMapper() {
+    final JSONata jsonata = JSONata.create(JSONataOptions.newBuilder()
+        .setObjectMapper(new ObjectMapper())
+        .build());
+    assertEquals(2, jsonata.parse("1 + 1").evaluate().intValue());
+  }
+
+  @Test
+  public void testCustomJsSource() throws Exception {
+    final String sourceString;
+    try (
+        InputStream sourceStream = new URL(
+            "https://cdn.jsdelivr.net/npm/jsonata@1.8.3/jsonata-es5.min.js").openStream();
+        Reader sourceReader = new InputStreamReader(sourceStream, UTF_8)) {
+      sourceString = readerToString(sourceReader);
+    }
+    final JSONata jsonata = JSONata.create(JSONataOptions.newBuilder()
+        .setJSONataJsSource(sourceString)
+        .build());
+    assertEquals(2, jsonata.parse("1 + 1").evaluate().intValue());
+  }
+
+  @Test
+  public void testInvalidJsSource() {
+    assertThrows(JSONataException.class, () -> JSONata.create(JSONataOptions.newBuilder()
+        .setJSONataJsSource("I am invalid")
+        .build()));
   }
 
 }
