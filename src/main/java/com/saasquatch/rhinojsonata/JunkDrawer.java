a5bd55c4bd4304b8d8fa23338a7c482b2c66e188
slisaasquatch <sli@saasquat.ch>
2021-05-12 19:59:48 -0700

    Update JunkDrawer.java

--- src/main/java/com/saasquatch/rhinojsonata/JunkDrawer.java
+++ src/main/java/com/saasquatch/rhinojsonata/JunkDrawer.java
@@ -14,7 +14,7 @@ import org.mozilla.javascript.Scriptable;
 
 final class JunkDrawer {
 
-  public static final String JS = "js", EVALUATE = "evaluate", ASSIGN = "assign",
+  public static final String EVALUATE = "evaluate", ASSIGN = "assign",
       REGISTER_FUNCTION = "registerFunction";
 
   /**
