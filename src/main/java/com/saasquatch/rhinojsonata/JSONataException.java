c0db42b4520cedf6ab9da62b83e779de0a0c897b
slisaasquatch <sli@saasquat.ch>
2021-05-13 12:32:43 -0700

    Update JSONataException.java

--- src/main/java/com/saasquatch/rhinojsonata/JSONataException.java
+++ src/main/java/com/saasquatch/rhinojsonata/JSONataException.java
@@ -2,18 +2,8 @@ package com.saasquatch.rhinojsonata;
 
 public final class JSONataException extends RuntimeException {
 
-  public JSONataException() {}
-
-  public JSONataException(String message) {
-    super(message);
-  }
-
   public JSONataException(String message, Throwable cause) {
     super(message, cause);
-  }
-
-  public JSONataException(Throwable cause) {
-    super(cause);
   }
 
 }
