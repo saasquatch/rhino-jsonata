e7e4369cc8035784a68b7a89cb95a2f67240fc9b
slisaasquatch <sli@saasquat.ch>
2021-05-12 20:35:00 -0700

    Update JSONataExpressionTests.java

--- src/test/java/com/saasquatch/rhinojsonata/JSONataExpressionTests.java
+++ src/test/java/com/saasquatch/rhinojsonata/JSONataExpressionTests.java
@@ -2,10 +2,12 @@ package com.saasquatch.rhinojsonata;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
 
 public class JSONataExpressionTests {
@@ -37,6 +39,30 @@ public class JSONataExpressionTests {
         JSONataExpression.parse("null").evaluate(JsonNodeFactory.instance.textNode("foo")));
     assertEquals(JsonNodeFactory.instance.objectNode(),
         JSONataExpression.parse("{\"foo\":bar}").evaluate(null));
+  }
+
+  @Test
+  public void testAssignJsExpression() {
+    final Supplier<JSONataExpression> supplier = () -> JSONataExpression.parse("$foo");
+    {
+      final JSONataExpression expression = supplier.get();
+      assertEquals(JsonNodeFactory.instance.missingNode(), expression.evaluate(null));
+    }
+    {
+      final JSONataExpression expression = supplier.get();
+      expression.assignJsExpression("foo", "1");
+      assertEquals(JsonNodeFactory.instance.numberNode(1.0), expression.evaluate(null));
+    }
+    {
+      final JSONataExpression expression = supplier.get();
+      expression.assignJsExpression("foo", "'1'");
+      assertEquals(JsonNodeFactory.instance.textNode("1"), expression.evaluate(null));
+    }
+    {
+      final JSONataExpression expression = supplier.get();
+      expression.assignJsExpression("foo", "a => a + a");
+      assertThrows(JSONataException.class, () -> expression.evaluate(null));
+    }
   }
 
 }
