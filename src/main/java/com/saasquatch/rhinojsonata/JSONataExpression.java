fd57aab9e7abe40cc110b271a13a82cf9b82d31f
slisaasquatch <sli@saasquat.ch>
2021-05-13 20:05:21 -0700

    add evaluate lock

--- src/main/java/com/saasquatch/rhinojsonata/JSONataExpression.java
+++ src/main/java/com/saasquatch/rhinojsonata/JSONataExpression.java
@@ -11,6 +11,8 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -25,6 +27,7 @@ import org.mozilla.javascript.json.JsonParser;
 
 public final class JSONataExpression {
 
+  private final Lock evaluateLock = new ReentrantLock();
   private final Context cx;
   private final Scriptable scope;
   private final ObjectMapper objectMapper;
@@ -55,8 +58,13 @@ public final class JSONataExpression {
         final String inputStringify = objectMapper.writeValueAsString(input);
         inputNativeObject = new JsonParser(cx, scope).parseValue(inputStringify);
       }
-      evaluateResult = ScriptableObject.callMethod(expressionNativeObject, EVALUATE,
-          new Object[]{inputNativeObject});
+      evaluateLock.lock();
+      try {
+        evaluateResult = ScriptableObject.callMethod(expressionNativeObject, EVALUATE,
+            new Object[]{inputNativeObject});
+      } finally {
+        evaluateLock.unlock();
+      }
     } catch (RhinoException e) {
       return rethrowRhinoException(cx, scope, e);
     } catch (JsonParser.ParseException | IOException e) {
