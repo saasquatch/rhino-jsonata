bf114a9bc0e9349c6cbe339e71db72f39fdb23e9
slisaasquatch <sli@saasquat.ch>
2021-05-13 19:47:20 -0700

    Update JSONataTests.java

--- src/test/java/com/saasquatch/rhinojsonata/JSONataTests.java
+++ src/test/java/com/saasquatch/rhinojsonata/JSONataTests.java
@@ -1,6 +1,7 @@
 package com.saasquatch.rhinojsonata;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,14 @@ public class JSONataTests {
   public void testJSONataTimeboxExpressionFunctionLazyInit() {
     final JSONata jsonata = JSONata.create();
     assertSame(jsonata.getTimeboxExpressionFunction(), jsonata.getTimeboxExpressionFunction());
+  }
+
+  @SuppressWarnings("ConstantConditions")
+  @Test
+  public void testParseExceptionHandling() {
+    final JSONata jsonata = JSONata.create();
+    assertThrows(NullPointerException.class, () -> jsonata.parse(null));
+    assertThrows(JSONataException.class, () -> jsonata.parse("\t"));
   }
 
 }
