682bd38e9062e0e98f088dd398817d69e2f350a5
slisaasquatch <sli@saasquat.ch>
2021-05-13 17:58:43 -0700

    Update JSONataExpression.java

--- src/main/java/com/saasquatch/rhinojsonata/JSONataExpression.java
+++ src/main/java/com/saasquatch/rhinojsonata/JSONataExpression.java
@@ -112,7 +112,6 @@ public final class JSONataExpression {
   }
 
   public void timeboxExpression(@Nonnull Duration timeout, @Nonnegative int maxDepth) {
-    Objects.requireNonNull(timeout);
     jsonata.getTimeboxExpressionFunction().call(cx, scope, scope,
         new Object[]{expressionNativeObject, (int) timeout.toMillis(), maxDepth});
   }
