73100b3fa751c95bbd9a0e8c3f5d4f39cba5dbe7
slisaasquatch <sli@saasquat.ch>
2021-05-17 21:30:18 -0700

    Update JunkDrawer.java

--- src/main/java/com/saasquatch/rhinojsonata/JunkDrawer.java
+++ src/main/java/com/saasquatch/rhinojsonata/JunkDrawer.java
@@ -89,7 +89,7 @@ final class JunkDrawer {
       } else {
         final String embeddedJsValueStringify = Optional.ofNullable(NativeJSON.stringify(
             cx, scope, embeddedJsValue, null, null).toString())
-            .filter(s -> !s.startsWith("org.mozilla.javascript.Undefined"))
+            .filter(s -> !s.startsWith(Undefined.class.getSimpleName()))
             .orElse(null);
         if (embeddedJsValueStringify != null) {
           try {
