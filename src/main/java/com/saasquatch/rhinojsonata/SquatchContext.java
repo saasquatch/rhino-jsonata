5c8d3bf9e6b717ef0b458dc74147202654c10c18
slisaasquatch <sli@saasquat.ch>
2021-05-29 15:09:17 -0700

    Update SquatchContext.java

--- src/main/java/com/saasquatch/rhinojsonata/SquatchContext.java
+++ src/main/java/com/saasquatch/rhinojsonata/SquatchContext.java
@@ -1,14 +1,23 @@
 package com.saasquatch.rhinojsonata;
 
+import org.mozilla.javascript.Callable;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
 
 final class SquatchContext extends Context {
 
+  /**
+   * ONLY to be set by {@link SquatchContextFactory#doTopCall(Callable, Context, Scriptable,
+   * Scriptable, Object[])}
+   */
   long startTimeNanos;
+  /**
+   * The execution timeout in nanos. Set it to a positive number to enforce the timeout.
+   */
   long timeoutNanos;
 
-  public SquatchContext(ContextFactory factory) {
+  SquatchContext(ContextFactory factory) {
     super(factory);
   }
 
