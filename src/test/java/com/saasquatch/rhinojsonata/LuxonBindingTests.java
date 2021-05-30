35f18b218744a2d5c970b4882c6c3670aafcbc82
slisaasquatch <sli@saasquat.ch>
2021-05-30 12:13:27 -0700

    Create LuxonBindingTests.java

---
+++ src/test/java/com/saasquatch/rhinojsonata/LuxonBindingTests.java
@@ -0,0 +1,56 @@
+package com.saasquatch.rhinojsonata;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Scriptable;
+
+public class LuxonBindingTests {
+
+  private static final String LUXON_JS_URL = "https://cdn.jsdelivr.net/npm/luxon@1.27.0/build/global/luxon.min.js";
+
+  private static JSONata jsonata;
+
+  @BeforeAll
+  public static void beforeAll() {
+    jsonata = JSONata.create();
+  }
+
+  @Test
+  public void test() throws Exception {
+    final Scriptable scope;
+    final Context cx = new ContextFactory().enterContext();
+    try (
+        InputStream luxonInputStream = new URL(LUXON_JS_URL).openStream();
+        Reader luxonReader = new InputStreamReader(luxonInputStream, UTF_8)
+    ) {
+      scope = cx.initSafeStandardObjects();
+      cx.evaluateReader(scope, luxonReader, "luxon.min.js", 1, null);
+      cx.evaluateString(scope, ""
+          + "function getProperties(clazz) {\n"
+          + "  return Object.getOwnPropertyNames(clazz)\n"
+          + "    .filter((p) => ![\"prototype\", \"name\", \"length\"].includes(p))\n"
+          + "    .reduce((props, p) => {\n"
+          + "      props[p] = clazz[p];\n"
+          + "      return props;\n"
+          + "    }, {});\n"
+          + "}", null, 1, null);
+    } finally {
+      Context.exit();
+    }
+    final JSONataExpression expression = jsonata.parse(
+        "$Duration.fromISO(\"P2M\").plus({\"months\":3, \"days\":10}).toISO()",
+        JSONataExpressionOptions.newBuilder().setScope(scope).build());
+    expression.assign("Duration", "getProperties(luxon.Duration)");
+    assertEquals("P5M10D", expression.evaluate().textValue());
+  }
+
+}
