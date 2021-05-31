d084b421bffdcfacd84694108aed0b317ecf7b7c
slisaasquatch <sli@saasquat.ch>
2021-05-30 20:59:17 -0700

    Update LuxonBindingTests.java

--- src/test/java/com/saasquatch/rhinojsonata/LuxonBindingTests.java
+++ src/test/java/com/saasquatch/rhinojsonata/LuxonBindingTests.java
@@ -10,7 +10,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Function;
 import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
 
 public class LuxonBindingTests {
 
@@ -46,6 +48,35 @@ public class LuxonBindingTests {
         "$Duration.fromISO(\"P2M\").plus({\"months\":3, \"days\":10}).toISO()",
         JSONataExpressionOptions.newBuilder().setScope(scope).build());
     expression.assign("Duration", "getProperties(luxon.Duration)");
+    assertEquals("P5M10D", expression.evaluate().textValue());
+  }
+
+  @Test
+  public void test2() throws Exception {
+    final Scriptable jsonataFriendlyLuxonDuration;
+    final Context cx = new ContextFactory().enterContext();
+    try (Reader luxonReader = new InputStreamReader(new URL(LUXON_JS_URL).openStream(), UTF_8)) {
+      final Scriptable scope = cx.initSafeStandardObjects();
+      cx.evaluateReader(scope, luxonReader, null, 1, null);
+      final Function getPropertiesFunction = (Function) cx.evaluateString(scope, ""
+          + "(clazz) => {\n"
+          + "  return Object.getOwnPropertyNames(clazz)\n"
+          + "    .filter((p) => ![\"prototype\", \"name\", \"length\"].includes(p))\n"
+          + "    .reduce((props, p) => {\n"
+          + "      props[p] = clazz[p];\n"
+          + "      return props;\n"
+          + "    }, {});\n"
+          + "}", null, 1, null);
+      final Scriptable luxon = (Scriptable) ScriptableObject.getProperty(scope, "luxon");
+      final Scriptable luxonDuration = (Scriptable) ScriptableObject.getProperty(luxon, "Duration");
+      jsonataFriendlyLuxonDuration = (Scriptable) getPropertiesFunction.call(
+          cx, scope, scope, new Object[]{luxonDuration});
+    } finally {
+      Context.exit();
+    }
+    final JSONataExpression expression = jsonata.parse(
+        "$Duration.fromISO(\"P2M\").plus({\"months\":3, \"days\":10}).toISO()");
+    expression.assign("Duration", jsonataFriendlyLuxonDuration);
     assertEquals("P5M10D", expression.evaluate().textValue());
   }
 
